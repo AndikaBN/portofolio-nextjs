fb7f3afd8d3bf0634a0339390498534232bcac3b
AndikaBN <bintangnursalih275@gmail.com>
2025-05-26 00:33:27 +0800

    Update build ID and manifest files; replace references to old build ID with new one, and add favicon.svg to the project.

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+      </head>
       <body className={`${inter.variable} ${spaceGrotesk.variable} font-sans`}>
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
           {children}
